456b2d81b69b02cdb15895a666f9bd0b09a799a2
Beyza <apple@MacBook-Pro.local>
2026-01-01 17:02:27 +0300

    Add prototypes and main skeleton

---
+++ include/prototypes.h
@@ -0,0 +1,19 @@
+#ifndef PROTOTYPES_H
+#define PROTOTYPES_H
+
+#include "structs.h"
+
+/* INITIALIZATION */
+void initialize(Type Types[18], Move Moves[486], Pokemon Pokemons[1015],
+                Player *Player1, Player *Player2);
+
+void initializeTypes(Type Types[18]);
+void initializeMoves(Move Moves[486], Type Types[18]);
+void initializePokemons(Pokemon Pokemons[1015], Type Types[18], Move Moves[486]);
+
+/* GAME */
+void game(Player *Player1, Player *Player2, Type Types[18]);
+void round(Player *Player1, Player *Player2, Type Types[18]);
+void applyDamage(Player *Player1, Player *Player2, Type Types[18]);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../include/prototypes.h"
+
+int main(void)
+{
+    Type Types[18];
+    Move Moves[486];
+    Pokemon Pokemons[1015];
+
+    Player Player1;
+    Player Player2;
+
+    initialize(Types, Moves, Pokemons, &Player1, &Player2);
+    game(&Player1, &Player2, Types);
+
+    return 0;
+}
